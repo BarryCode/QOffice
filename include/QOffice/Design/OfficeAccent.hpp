71f66945d5159051be08f8b68bb244420905c3e7
Nicolas Kogler <nicolas.kogler@hotmail.com>
2017-09-23 18:42:30 +0200

    Add a class to receive accent colors using an enum value.

---
+++ include/QOffice/Design/OfficeAccent.hpp
@@ -0,0 +1,163 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// QOffice - The office framework for Qt
+// Copyright (C) 2016-2018 Nicolas Kogler
+//
+// This file is part of the Design module.
+//
+// QOffice is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// QOffice is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with QOffice. If not, see <http://www.gnu.org/licenses/>.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+#ifndef QOFFICE_DESIGN_OFFICEACCENT_HPP
+#define QOFFICE_DESIGN_OFFICEACCENT_HPP
+
+#include <QOffice/Widgets/OfficeWidget.hpp>
+#include <QColor>
+
+////////////////////////////////////////////////////////////////////////////////
+/// \class OfficeAccent
+/// \brief Holds all the color values of the available accents.
+/// \author Nicolas Kogler (nicolas.kogler@hotmail.com)
+/// \date September 23, 2017
+///
+////////////////////////////////////////////////////////////////////////////////
+class QOFFICE_DESIGN_API OfficeAccent
+{
+public:
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Retrieves the color associated with the specified accent.
+    ///
+    /// \param[in] accent The accent name of the color to get.
+    /// \return The color associated with the given accent.
+    ///
+    /// \sa OfficeAccent::setCustomColor
+    /// \threadsafe Only call this function on the GUI thread.
+    /// \throws OfficeAccentException
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    static const QColor& color(OfficeWidget::Accent accent);
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Retrieves a color being a bit lighter than the color associated with the
+    /// specified accent.
+    ///
+    /// \param[in] accent The accent name of the color to get.
+    /// \return A color lighter than the one associated with the given accent.
+    ///
+    /// \sa OfficeAccent::veryLightColor
+    /// \threadsafe Only call this function on the GUI thread.
+    /// \throws OfficeAccentException
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    static const QColor lightColor(OfficeWidget::Accent accent);
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Retrieves a color being much lighter than the color associated with the
+    /// specified accent.
+    ///
+    /// \param[in] accent The accent name of the color to get.
+    /// \return A color much lighter than the one associated with the given accent.
+    ///
+    /// \sa OfficeAccent::lightColor
+    /// \threadsafe Only call this function on the GUI thread.
+    /// \throws OfficeAccentException
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    static const QColor veryLightColor(OfficeWidget::Accent accent);
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Retrieves a color being a bit darker than the color associated with the
+    /// specified accent.
+    ///
+    /// \param[in] accent The accent name of the color to get.
+    /// \return A color darker than the one associated with the given accent.
+    ///
+    /// \sa OfficeAccent::veryDarkColor
+    /// \threadsafe Only call this function on the GUI thread.
+    /// \throws OfficeAccentException
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    static const QColor darkColor(OfficeWidget::Accent accent);
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Retrieves a color being much darker than the color associated with the
+    /// specified accent.
+    ///
+    /// \param[in] accent The accent name of the color to get.
+    /// \return A color much darker than the one associated with the given accent.
+    ///
+    /// \sa OfficeAccent::darkColor
+    /// \threadsafe Only call this function on the GUI thread.
+    /// \throws OfficeAccentException
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    static const QColor veryDarkColor(OfficeWidget::Accent accent);
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Determines whether the given accent is a valid accent within QOffice.
+    ///
+    /// \param[in] accent The accent to check.
+    /// \return True if the given \p accent is valid, false otherwise.
+    ///
+    /// \threadsafe This function is thread-safe.
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    static bool isValid(OfficeWidget::Accent accent);
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// Specifies the color of the custom accent.
+    ///
+    /// \param[in] color The color to specify for the custom accent.
+    ///
+    /// \sa OfficeAccent::color
+    /// \threadsafe Only call this function on the GUI thread
+    ///
+    ////////////////////////////////////////////////////////////////////////////
+    static void setCustomColor(const QColor& color);
+
+private:
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Static members
+    //
+    ////////////////////////////////////////////////////////////////////////////
+     ///< Holds all accent colors.
+};
+
+#endif
+
+////////////////////////////////////////////////////////////////////////////////
+/// \class OfficeAccent
+/// \ingroup Design
+///
+/// Every QOffice-widget should use the functionality of this class in their
+/// QWidget::paintEvent method. Use as follows:
+///
+/// \code
+/// void paintEvent(QPaintEvent*)
+/// {
+///     QPainter painter(this);
+///     const QColor& accentColor = OfficeAccent::color(accent());
+///     painter.fillRect(rect(), accentColor);
+/// }
+/// \endcode
+///
+/// If you always use the virtual OfficeWidget::accent method, you do not need
+/// to handle exceptions, as OfficeWidget::setAccent always ensures that the
+/// provided accent is in range 0 <= x <= OfficeWidget::CustomAccent.
+///
+////////////////////////////////////////////////////////////////////////////////
---
+++ src/Design/OfficeAccent.cpp
@@ -0,0 +1,100 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// QOffice - The office framework for Qt
+// Copyright (C) 2016-2018 Nicolas Kogler
+//
+// This file is part of the Design module.
+//
+// QOffice is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// QOffice is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with QOffice. If not, see <http://www.gnu.org/licenses/>.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include <QOffice/Design/OfficeAccent.hpp>
+#include <QOffice/Exceptions/OfficeAccentException.hpp>
+
+OffAnonymous(QColor g_colors[OfficeWidget::CustomAccent+1])
+
+const QColor& OfficeAccent::color(OfficeWidget::Accent accent)
+{
+    if (!isValid(accent))
+    {
+        throw OfficeAccentException(OffCurrentFunc, static_cast<int>(accent));
+    }
+
+    return g_colors[accent];
+}
+
+const QColor OfficeAccent::lightColor(OfficeWidget::Accent accent)
+{
+    if (!isValid(accent))
+    {
+        throw OfficeAccentException(OffCurrentFunc, static_cast<int>(accent));
+    }
+
+    return g_colors[accent].lighter(130);
+}
+
+const QColor OfficeAccent::veryLightColor(OfficeWidget::Accent accent)
+{
+    if (!isValid(accent))
+    {
+        throw OfficeAccentException(OffCurrentFunc, static_cast<int>(accent));
+    }
+
+    return g_colors[accent].lighter(200);
+}
+
+const QColor OfficeAccent::darkColor(OfficeWidget::Accent accent)
+{
+    if (!isValid(accent))
+    {
+        throw OfficeAccentException(OffCurrentFunc, static_cast<int>(accent));
+    }
+
+    return g_colors[accent].darker(130);
+}
+
+const QColor OfficeAccent::veryDarkColor(OfficeWidget::Accent accent)
+{
+    if (!isValid(accent))
+    {
+        throw OfficeAccentException(OffCurrentFunc, static_cast<int>(accent));
+    }
+
+    return g_colors[accent].darker(200);
+}
+
+bool OfficeAccent::isValid(OfficeWidget::Accent accent)
+{
+    auto value = static_cast<int>(accent);
+
+    // The custom accent will _always_ be the last accent, so we can safely
+    // determine whether 0 <= value <= CustomAccent.
+    return value >= 0 && value <= OfficeWidget::CustomAccent;
+}
+
+void OfficeAccent::setCustomColor(const QColor& color)
+{
+    g_colors[OfficeWidget::CustomAccent] = color;
+}
+
+QColor g_colors[OfficeWidget::CustomAccent+1] =
+{
+    QColor(0x2b579a),
+    QColor(0xa4373a),
+    QColor(0x217346),
+    QColor(0xb83B1d),
+    QColor(0x68217a),
+    QColor(0xf00bae),
+};
